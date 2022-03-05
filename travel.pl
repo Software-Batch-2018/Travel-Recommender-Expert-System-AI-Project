a8ff71a89fdcaf63ecfbc2a8544fd0df30a5743e
kusaljr <prastutlamsal41@gmail.com>
2022-03-05 22:36:34 +0545

    AI PROJECT

---
+++ travel.pl
@@ -0,0 +1,158 @@
+main :- intro, find_place(Place), describe(Place), nl, write('*** HAPPY JOURNEY ***'), nl, nl.
+
+intro :- write('Welcome to TOURISM EXPERT SYSTEM.'), nl.
+
+
+describe(pokhara) :- write('POKHARA... '), nl, write('Pokhara, the Tourist Capital of Nepal, is the second-largest city in this Himalayan country, after Kathmandu. At an altitude of more than 900m, it is one of the highest cities, making it the base for many world-famous treks. The highlight of the town is its Lakeside, which is a boating paradise flanked with charming shops, dreamy cafés, restaurants and pubs that make up this postcard-perfect town.
+For adventure enthusiasts, Pokhara is a popular starting point for many treks in the Annapurna Range as well as many paragliding and river rafting opportunities. You can take solo or tandem rides, depending on your expertise. River rafting and white water rafting are offered on Seti Gandaki and her tributaries.
+The city is a beautiful contrast of the bustling Lakeside Pokhara and Old Pokhara, which is home to many beautiful historical temples and is also the citys commercial hub. Phewa Lake (or Phewa Tal), a spectacular lake that borders Lakeside Pokhara and Old Pokhara, it flawlessly reflects the mountains that border it, making it a breathtaking photo-op. The old city houses shops where you can buy famous handicrafts, woodwork and woolwork. Dont forget to visit the Tibetan Mini Market for one-of-a-kind souvenirs!'),nl.
+
+describe(kathmandu) :- write('KATHMANDU ...'), nl, write('Kathmandu, the city of peace, lives true to this description. The city is charming, busy, bustling, serene, alive and asleep - all at once. The capital of Nepal, Kathmandu is the most accessible city in the country - it is Nepals only metropolitan city as well. From buying trekking equipment in Thamel or taking a rickshaw ride through traffic-packed alleys of the old part of the city or simply visiting the fascinating buildings in Durbar Squares, Kathmandu is truly a place filled with warmth.
+Kathmandu is home to more than 1.5 million people. The city is at an altitude of a whopping 1400 metres or 4600 feet, which assures a blissful climate, all year long. Kathmandu, which is derived from the Pahari word for wood shelter, is an abode of serenity and tranquillity, with its monasteries, temples and spiritual melting pots. While the natural beauty of the city is more than enough for travellers to throng Kathmandu year after year, what sets it apart from other tourist destinations is the variety it offers - from spiritual awakening to adventure sports.'),nl.
+
+describe(mustang) :- write('MUSTANG ...'), nl, write('Mustang is known for its trekking tours and mountain sightseeing. Elevated at an altitude of 3840 kilometres, this city is divided into two parts: Lower and Upper Mustang. Even though it is relatively unexplored, this city is a great destination for those who want to immerse in Tibetan culture as well as in Nepalese lifestyle as it is located near the Tibetan Plateau.
+Lower Mustang is well-known for natural scenery while upper Mustang is famous for its trekking and hiking trails, monasteries, caves and local tribes. This destination is rich in vegetation with a range of rhododendron plantations, apple fields and is strong with Buddhist culture. Visitors get to see the lifestyle of the mountain people, their day-to-day routines and their customs. Visitors can take a look at a number of Tibetan shrines, monasteries, caves and villages in order to learn about the “Mini Tibet in Nepal”. All in all, this North-West part of Nepal has a rugged and mystical reputation and is a great destination for backpackers, trekkers or those who want a vacation away from the commercial life and want to spend some time in the Himalayas.'),nl.
+
+describe(rasuwa) :- write('RASUWA ...'), nl, write('Natural resources abound in Rasuwa. To the north of Rasuwa is the Langtang mountain range. Langtang National Park encompasses a significant portion of the northern part of the area. Rasuwas tourism highlights include Gosainkunda Lake, Ganja La Pass, and Tamang village in Bridim.
+In the Langtang district, the Gosainkunda Lake, also known as "Frozen Lake," is one of the most beautiful mountain lakes. In this region, there are approximately 108 kundas (lakes). The most popular ones are Saraswati Kund, Bhairab Kund, Surya Kund, and Gosainkund.
+The Langtang valley, aptly named the valley of glaciers, is another draw in Rasuwa, where mountains climb soaring into the sky. Pine forests, swift mountain streams, rocky granite and snow-capped hills, grassy downs and meadows are strewn with daisies, and wild animals can all be seen in the valley.'), nl.
+
+describe(khaptad) :- write('KHAPTAD ...'), nl, write('Khaptad National Park is the protected area lies in the far-western region of Nepal. The park covers an area of 225km2 (87 sq mi) stretching over four amazing districts Bajhang, Bajura, Achham, and Doti. While it ranges an elevation from 1,400m (4,600ft) to the highest 3,300m (10,800ft). Khaptad National Park was established in 1984 on the advice of Khaptad Baba who was the holy man of this region. The renowned Khaptad Baba Ashram is now a religious site for Hindu pilgrims especially July and August full moon day. The other important place of religious attractions are Sahasralingam Mandir, Nag Dhunga, Triveni, and the Khaptad Lake. Overall, Khaptad National Park is one of the holiest national parks in Nepal.
+Khaptad National Park connects the heart of the nature lovers. The park has 355 species of vascular plants. Vegetation inside this park includes chir-pine, Rhododendron forest, oak forest and Himalayan fir-hemlock-oak forest and alder forest in reverie areas. Among 23 mammalian species of the park are leopards, Himalayan black bear, wild dog and musk deer. And also the park is home to 287 bird species with both migratory and residents. Some of the bird symbolic of the park are implying pheasant, Peregrine falcon, and vulture.'), nl.
+
+describe(bardia_national_park) :- write('BARDIYA NATIONAL PARK ...'), nl, write('Bardiya National park is the largest national park in the lowland Terai covering an area of 968 sq.km. The park situated in Nepals Western Terai was established for protecting the representative ecosystems and conserving the habitat of tiger and its prey species. Initially, a small area was gazetted as the Karnali Wildlife Reserve in 1976. At that time, 1500 households of the Babai valley were relocated outside the park allowing the vegetation and wildlife to flourish. In 1982, it was renamed as Bardiya Wildlife Reserve, and in 1984 it was extended to its current size. The reserve was given the status of a National Park in 1988. Greater One-horned Rhinoceros were translocated from Chitwan National Park to Bardia National Park in 1986, 1991, 1999, 2000, 2001 and 2002. In 1997, an area of 327 km2 surrounding the park was declared as a buffer zone, which consists of forests and private lands. The park and local communities jointly manage the buffer zone. Together they initiate community development activities and manage natural resources in the buffer zones. An elephant ride provides a different view of the park as one can go off the main trail, Morning and late afternoon is the ideal time to go on a ride. Karnali river is the suitable home for Gangetic dolphin. Babai valley is a majestic place to visit where flagship Rhino, tiger, and elephant can be cited in the wilderness site.'), nl.
+
+describe(annapurna_circuit) :- write('ANNAPURNA CIRCUIT ...'),nl, write('The Annapurna Circuit is one of the greatest treks in Nepal, if not the world. This classic circuit trek is an incredibly varied and extraordinarily beautiful journey. Dramatic deep gorges, bleak high mountain passes, Buddhist temples, picturesque farming villages and all the major mountains of the Annapurna Massif.
+To reach the start of the trek, we drive first down the main Pokhara road from Kathmandu and then follow the Marshyangdi River to Besisahar. The trek proper begins here, crossing and re-crossing the gorge carved by the Marshyangdi River on suspension bridges before you reach Manang, the largest village on the route. An acclimatisation day is taken here to prepare for crossing the Thorung La pass which is the highest point of the trek at 5416m. From there you descend progressively with plenty of up and down along the way until you reach Jomsom. We end our trip with a spectacular mountain flight to Pokhara and then a second flight on to Kathmandu.'), nl.
+
+describe(solukhumbu) :- write('SOLUKHUMBU ...'),nl,write('Solukhumbu District is famous in the world for Mountain tourism. The main attraction of this district is Mount Everest(8,848m). UNESCO has listed the Sagarmatha National Park in Wolrd Heritage sites. The Sherpa culture, famous Tengboche Monastery are the highlights of Everest. Namche Bazaar is the entrance gate of Everest. The flora and fauna of this region are of an alpine type. It is the land for adventure travel holidays. Mount Everest Expedition, Everest Base Camp Trek, Everest View Trek, Island Peak Climbing are the popular trekking trail in Everest of Solukhumbu. Solukhumbu is the best destination for Mountaineering, Peak Climbing and Trekking Holidays.'),nl.
+
+describe(bhaktapur) :- write('Bhaktapur is one of three medieval cities in the Kathmandu Valley, the other two being Kathmandu and Patan. It is often referred to as Bhadgaon and Khwopa and has a mix of Hindu and Buddhist population. This city is located around 12 kilometres from the capital city of Kathmandu and is divided into three squares, each of them filled with temples and other religious structures. Entry to this town, however, is not free and visitors need to pay a town entry fee of NPR 1500. This goes into the maintenance of the temples in Bhaktapur.
+A place for devotees, this city is located in the Eastern part of Kathmandu valley. Bhaktapur, “the city of culture” was unfortunately destroyed during the 7.9 magnitude earthquake in April 2015, causing loss of life and terrible devastation of some of the historically important temples. Nevertheless, the city has been getting back on its feet steadily over the past 4 years and as a result, is one of the most valuable historical cities in Nepal. 
+This city is placed on the trade route between India and Tibet and gives a clear view of the Himalayas. Being the third-largest city in Nepal suggests that Bhaktapur is one of the popular tourist destinations with Bhaktapur Durbar Square being the most common attraction.'), nl.
+
+find_place(Place) :- place(Place).
+
+
+
+answer(religious) :- write('Religious').
+answer(wildlife) :- write('Wildlife').
+answer(fun) :- write('Fun').
+answer(history) :- write('History').
+answer(hinduism) :- write('Hinduism').
+answer(buddhism) :- write('Buddhism').
+answer(himalaya) :- write('Himalayan Wildlife').
+answer(hilly) :- write('Hilly Wildlife').
+answer(terai) :- write('Terai Region Wildlife').
+answer(monuments) :- write('Historical Monuments').
+answer(adventure) :- write('Adventure Related').
+answer(scenary) :- write('Scenaries and Nature').
+answer(trekking) :- write('Trekking').
+answer(backpacking) :- write('Backpacking').
+answer(mountain) :- write('Mountains').
+answer(lake) :- write('Lakes').
+answer(snow) :- write('Snowfall').
+answer(cultures) :- write('Historic Cultures').
+answer(heritage) :- write('Heritage').
+
+
+
+place(pokhara) :- trip_purpose(fun), fun_type(scenary), scenary_type(lake).
+
+place(kathmandu) :- trip_purpose(history), (history_type(monuments); history_type(heritage); history_type(cultures)).
+
+place(mustang) :- trip_purpose(fun), fun_type(scenary), (scenary_type(snow)).			
+place(mustang) :- trip_purpose(history), history_type(monuments).
+
+place(rasuwa) :- trip_purpose(fun), fun_type(scenary), scenary_type(lake).
+
+place(khaptad) :- trip_purpose(wildlife), wildlife_region(hilly).
+place(khaptad) :- trip_purpose(fun), fun_type(adventure), (adventure_type(backpacking) ).
+
+
+place(bardia_national_park) :- trip_purpose(wildlife), wildlife_region(terai).
+
+place(annapurna_circuit) :- trip_purpose(fun), fun_type(adventure), (adventure_type(trekking)).
+
+place(solukhumbu) :- trip_purpose(wildlife), wildlife_region(himalaya).
+place(solukhumbu) :- trip_purpose(fun), fun_type(scenary), ( scenary_type(mountain); scenary_type(snow) ).			
+
+place(bhaktapur) :- trip_purpose(history), history_type(heritage).
+
+question(trip_purpose) :- write('What kind of places would you like to see ?'), nl.
+question(religion_name) :- write('Interested in which religion ?'), nl.
+question(wildlife_region) :- write('Which wildlife region would you like to see ?'), nl.
+question(history_type) :- write('What would you like to see ?'), nl.
+question(fun_type) :- write('Choose the option you are interested in ?'), nl.
+question(adventure_type) :- write('What kind of adventure ?'), nl.
+question(water_type) :- write('Type your choice ?'), nl.
+question(scenary_type) :- write('What would you like to see ?'), nl.
+question(entertainment_type) :- write('What kind of entertainment would you like ?'), nl.
+
+
+:- dynamic(progress/2).
+    
+trip_purpose(Answer) :-
+  progress(trip_purpose, Answer).
+trip_purpose(Answer) :-
+  \+ progress(trip_purpose, _),
+  ask(trip_purpose, Answer, [wildlife, fun, history]).    
+
+
+wildlife_region(Answer) :- 
+    progress(wildlife_region, Answer).
+wildlife_region(Answer) :- 
+    \+ progress(wildlife_region, _),
+    ask(wildlife_region, Answer, [himalaya, hilly, terai]).
+
+history_type(Answer) :-
+  progress(history_type, Answer).
+history_type(Answer) :-
+  \+ progress(history_type, _),
+  ask(history_type, Answer, [monuments, cultures, heritage]).
+
+fun_type(Answer) :-
+  progress(fun_type, Answer).
+fun_type(Answer) :-
+  \+ progress(fun_type, _),
+  ask(fun_type, Answer, [adventure, scenary]).
+
+adventure_type(Answer) :-
+  progress(adventure_type, Answer).
+adventure_type(Answer) :-
+  \+ progress(adventure_type, _),
+  ask(adventure_type, Answer, [trekking, backpacking]).
+
+scenary_type(Answer) :-
+  progress(scenary_type, Answer).
+scenary_type(Answer) :-
+  \+ progress(scenary_type, _),
+  ask(scenary_type, Answer, [mountain, lake, snow]).
+
+
+% Outputs a nicely formatted list of answers
+% [First|Rest] is the Choices list, Index is the index of First in Choices
+answers([], _).
+answers([First|Rest], Index) :-
+  write(Index), write(' '), answer(First), nl,
+  NextIndex is Index + 1,
+  answers(Rest, NextIndex).
+
+
+% Parses an Index and returns a Response representing the "Indexth" element in
+% Choices (the [First|Rest] list)
+parse(0, [First|_], First).
+parse(Index, [First|Rest], Response) :-
+  Index > 0,
+  NextIndex is Index - 1,
+  parse(NextIndex, Rest, Response).
+
+
+% Asks the Question to the user and saves the Answer
+ask(Question, Answer, Choices) :-
+  question(Question),
+  answers(Choices, 0),
+  read(Index),
+  parse(Index, Choices, Response),
+  asserta(progress(Question, Response)),
+  Response = Answer.
